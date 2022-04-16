5ac6212898a2f4d109369c531994865a62fa1750
Maxence Thevenet <maxence.thevenet@desy.de>
2022-04-16 10:24:04 +0200

    implement double binning

--- src/particles/TileSort.cpp
+++ src/particles/TileSort.cpp
@@ -18,6 +18,7 @@ findParticlesInEachTile (
     HIPACE_PROFILE("findParticlesInEachTile()");
 
     // Tile box: only 1 cell longitudinally, same as bx transversally.
+    const int s = tile_size / bin_size;
     const amrex::Box tcbx = bx.coarsen(bin_size);
     const amrex::Box cbx = {{tcbx.smallEnd(0),tcbx.smallEnd(1),0}, {tcbx.bigEnd(0),tcbx.bigEnd(1),0}};
 
@@ -30,25 +31,26 @@ findParticlesInEachTile (
 
         // Extract box properties
         const auto lo = lbound(cbx);
+        const auto hi = ubound(cbx);
         const auto dxi = amrex::GpuArray<amrex::Real,AMREX_SPACEDIM>({
                 geom.InvCellSizeArray()[0]/bin_size,
                 geom.InvCellSizeArray()[1]/bin_size,
                 1.});
         const auto plo = geom.ProbLoArray();
-
+        const int nx = hi.x-lo.x+1;
+        const int ny = hi.y-lo.y+1;
+        
         // Find the particles that are in each tile and return collections of indices per tile.
         bins.build(
-            pti.numParticles(), pti.GetArrayOfStructs().begin(), cbx,
+            pti.numParticles(), pti.GetArrayOfStructs().begin(), nx*ny,
             // Pass lambda function that returns the tile index
             [=] AMREX_GPU_DEVICE (const PlasmaParticleContainer::ParticleType& p)
-            noexcept -> amrex::IntVect
+            noexcept -> int
             {
-                return amrex::IntVect(
-                    AMREX_D_DECL(
-                        static_cast<int>((p.pos(0)-plo[0])*dxi[0]-lo.x),
-                        static_cast<int>((p.pos(1)-plo[1])*dxi[1]-lo.y),
-                        0));
-            }, tile_size/bin_size);
+                const int ix = static_cast<int>((p.pos(0)-plo[0])*dxi[0]-lo.x);
+                const int iy = static_cast<int>((p.pos(1)-plo[1])*dxi[1]-lo.y);
+                return (ix/s) * s * ny + (iy/s) * s * s + (iy%s) + (ix%s)*s;
+            });
     }
     AMREX_ALWAYS_ASSERT(count <= 1);
     return bins;
